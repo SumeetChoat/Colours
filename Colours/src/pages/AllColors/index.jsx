1300a87d28df727553aea55ddfdfe86b158bd858
“SumeetChoat” <sumeetpalchoat@hotmail.co.uk>
2023-07-12 12:37:05 +0100

    added many files

---
+++ Colours/src/pages/AllColors/index.jsx
@@ -0,0 +1,28 @@
+function Home() {
+    return (
+        <div>
+            <h1>All Colors</h1>
+            <p>Below are a list of Colors</p>
+        </div>
+    )
+}
+
+const colors = [
+    { name: "Cerulean", code: "#007BA7" },
+    { name: "Magenta", code: "#FF00FF" },
+    { name: "Turquoise", code: "#40E0D0" },
+    { name: "Indigo", code: "#4B0082" },
+    { name: "Crimson", code: "#DC143C" },
+    { name: "Amber", code: "#FFBF00" },
+    { name: "Lavender", code: "#E6E6FA" },
+    { name: "Emerald", code: "#50C878" },
+    { name: "Maroon", code: "#800000" },
+    { name: "Saffron", code: "#F4C430" },
+    { name: "Teal", code: "#008080" },
+    { name: "Rose", code: "#FF007F" },
+    { name: "Periwinkle", code: "#CCCCFF" },
+    { name: "Olive", code: "#808000" },
+    { name: "Coral", code: "#FF7F50" },
+  ];
+
+export default Home;
